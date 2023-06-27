3d87d5df637be609a7ad5388ff75a8ba11e5bc3a
iamseun <seunayela@gmail.com>
2023-06-28 00:39:58 +0100

    changed task 9 from innerIncome to income

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,11 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(innerIncome) {
-      return `$${innerIncome}`;
+    getIncomeInDollars(income) {
+      return `$${income}`;
     },
-    getIncomeInEuros(innerIncome) {
-      return `${innerIncome} euros`;
+    getIncomeInEuros(income) {
+      return `${income} euros`;
     },
   };
   return fullBudget;

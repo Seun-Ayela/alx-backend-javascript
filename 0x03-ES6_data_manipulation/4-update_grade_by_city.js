b1aee6c5a53290d383cfca3c761816783afe6654
iamseun <seunayela@gmail.com>
2023-07-05 01:11:16 +0100

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-param-reassign */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const filterStudents = students.filter(
+    (student) => student.location === city,
+  );
+
+  const gradeStudents = filterStudents.map((student) => {
+    for (const gradeInfo of newGrades) {
+      if (student.id === gradeInfo.studentId) {
+        student.grade = gradeInfo.grade;
+      }
+    }
+    if (student.grade === undefined) {
+      student.grade = 'N/A';
+    }
+    return student;
+  });
+
+  return gradeStudents;
+}
